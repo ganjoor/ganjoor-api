3f74dab408dcffbcc8ec70369ea247babba4ea5d
Sallar Kaboli <sallar.kaboli@gmail.com>
2017-02-27 19:59:45 +0200

    Add Docker

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,12 +1,7 @@
 import * as Sequelize from 'sequelize';
 require('sequelize-hierarchy')(Sequelize);
 
-const DB_USER = process.env.DB_USER || 'root';
-const DB_PASS = process.env.DB_PASS || '';
-
-export const sequelize = new Sequelize('ganjoor', DB_USER, DB_PASS, {
-  host: 'localhost',
-  dialect: 'mysql'
-});
+const DB_URI = process.env.MYSQL_URL || 'mysql://root:@localhost:3306/ganjoor';
+export const sequelize = new Sequelize(DB_URI);
 
 export const connect = () => sequelize.sync();
---
+++ typings/index.d.ts
@@ -0,0 +1,1 @@
+/// <reference path="modules/sequelize/index.d.ts" />
