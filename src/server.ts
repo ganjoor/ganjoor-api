02aadc4d6e3f2b12458d3bd7ddd433e289f130a4
Sallar Kaboli <sallar.kaboli@gmail.com>
2017-03-04 17:30:27 +0200

    Add environment variables for dev

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import * as express from 'express';
-import * as jwt from 'express-jwt';
 import * as finalhandler from 'finalhandler';
 import * as cors from 'cors';
+import authenticate from './utils/auth';
 import { logger } from './utils/logger';
 import { router as swaggerRouter } from './swagger';
 import { router as poetryRouter } from './poetry';
@@ -12,13 +12,6 @@ const app = express();
 
 app.use(cors());
 app.disable('x-powered-by');
-
-const authenticate = jwt({
-  secret: process.env.AUTH0_CLIENT_SECRET,
-  audience: process.env.AUTH0_CLIENT_ID,
-  issuer: `https://${process.env.AUTH0_DOMAIN}/`,
-  algorithms: ['HS256']
-});
 
 app.get('/', (req, res) => {
   res.json({
